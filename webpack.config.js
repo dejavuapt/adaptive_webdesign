50c24f98e8212a52e68ac2b5cdfcb9ed23a3af38
dejavuapt <thepoli.b@gmail.com>
2022-04-29 14:09:45 +0500

    i don't know is

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname,'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+}
